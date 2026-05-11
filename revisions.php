6f4a4c39ed72b4c2d01bd1eddf09945424bdb034
Anthony L <laplane_anthony@yahoo.fr>
2026-05-11 19:32:51 +0200

    live 2

---
+++ revisions.php
@@ -0,0 +1,9 @@
+<?php
+
+$age = 20;
+
+
+?>
+<h1>Révisions</h1>
+<h2><?php echo $age; ?></h2>
+<h2><?=$age; ?></h2>
---
+++ templates/header.php
@@ -0,0 +1,37 @@
+<?php
+
+$mainMenu = [
+    "index.php" => "Accueil",
+    "a-propos.php" => "A propos"
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/override-bootstrap.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0"> <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="/assets/images/spendly.svg" alt="Logo spendly" width="150">
+                </a> </div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <?php foreach ($mainMenu as $page => $title) { ?>
+                        <li><a href="<?= $page ?>" class="nav-link px-2"><?=$title ?></a></li>
+                <?php } ?>
+            </ul>
+            <div class="col-md-3 text-end"> <button type="button" class="btn btn-outline-primary me-2">Login</button> <button type="button" class="btn btn-primary">Sign-up</button> </div>
+        </header>
+    </div>
+
+    <main>
